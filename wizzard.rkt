8abc53f26aeb14185769224ef2d15d93fb9d4659
Alexey Zlobin <alexey.zlobin@gmail.com>
2013-11-16 19:58:12 +0400

    graphviz output

--- wizzard.rkt
+++ wizzard.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+(require scheme/system)
+
 (define *nodes*
   '((living-room (you are in the living-room. a wizard is snoring loudly on the couch.))
     (garden (you are in a beautiful garden. there is a well in front of you.))
@@ -115,4 +117,47 @@
       (game-print (game-eval cmd))
       (game-repl))))
 
+(define (dot-name exp)
+  (regexp-replace* #px"([^a-z0-9])" (write-to-string exp) "_"))
 
+(define (dot-label exp)
+  (let ([full-name (write-to-string exp)])
+    (if (> (string-length full-name) 30)
+        (string-append (substring full-name 0 27) "...")
+        full-name)))
+
+(define (nodes->dot nodes)
+  (map (lambda (node)
+         (display "\n")
+         (display (dot-name (car node)))
+         (display "[label=\"")
+         (display (dot-label node))
+         (display "\"];"))
+       nodes))
+
+(define (edges->dot edges)
+  (map (lambda (node)
+         (map (lambda (edge)
+                (display "\n")
+                (display (dot-name (car node)))
+                (display " -> ")
+                (display (dot-name (car edge)))
+                (display "[label=\"")
+                (display (dot-label (cdr edge)))
+                (display "\"];"))
+              (cdr node)))
+       edges))
+
+(define (graph->dot nodes edges)
+  (display "digraph {")
+  (nodes->dot nodes)
+  (edges->dot edges)
+  (display "\n}"))
+
+(define (dot->png fname thunk)
+  (with-output-to-file fname thunk #:mode 'text #:exists 'replace)
+  (system (string-append "dot -Tpng -O " fname)))
+
+(define (graph->png nodes edges fname)
+  (dot->png fname
+           (lambda () (graph->dot nodes edges))))
